51af5c1b6c6b8031d9f9f45deafaa321e1a7a2b5
richard <richard.zsupos@gmail.com>
2024-01-01 23:05:26 +0100

    Kids With The Greatest Number Of Candies

---
+++ LeetCode75.Main/ArraysAndStrings/KidsWithTheGreatestNumberOfCandies.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode75.Main.ArraysAndStrings;
+
+public class KidsWithTheGreatestNumberOfCandies
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        int greatest = candies.Max();
+        List<bool> result = [];
+
+        for (int index = 0; index < candies.Length; index++) 
+        {
+            if (candies[index] + extraCandies < greatest)
+            {
+                result.Add(false);
+            }
+            else
+            {
+                result.Add(true);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode75.Tests/ArraysAndStrings/KidsWithTheGreatestNumberOfCandiesTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode75.Main.ArraysAndStrings;
+
+namespace LeetCode75.Tests.ArraysAndStrings;
+
+[TestFixture]
+internal class KidsWithTheGreatestNumberOfCandiesTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] candies, int extraCandies, bool[] expected)
+    {
+        Assert.That(new KidsWithTheGreatestNumberOfCandies().KidsWithCandies(candies, extraCandies), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 2, 3, 5, 1, 3 }, 3, new bool [] { true, true, true, false, true });
+        yield return new TestCaseData(new int[] { 4, 2, 1, 1, 2 }, 1, new bool [] { true, false, false, false, false });
+        yield return new TestCaseData(new int[] { 12, 1, 12 }, 10, new bool [] { true, false, true });
+    }
+}

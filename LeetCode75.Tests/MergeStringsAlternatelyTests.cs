fed30a03d592a2079257ba91ae0391a25a34c30d
richard <richard.zsupos@gmail.com>
2024-01-01 18:46:13 +0100

    first commit

---
+++ LeetCode75.Tests/MergeStringsAlternatelyTests.cs
@@ -0,0 +1,16 @@
+using LeetCode75.Main;
+
+namespace LeetCode75.Tests
+{
+    public class MergeStringsAlternatelyTests
+    {
+
+        [TestCase("abc", "pqr", "apbqcr")]
+        [TestCase("ab", "pqrs", "apbqrs")]
+        [TestCase("abcd", "pq", "apbqcd")]
+        public void TestExamples(string word1, string word2, string expected)
+        {
+            Assert.That(new MergeStringsAlternately().MergeAlternately(word1, word2), Is.EqualTo(expected));
+        }
+    }
+}

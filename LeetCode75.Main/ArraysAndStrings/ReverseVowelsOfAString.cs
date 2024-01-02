bf0c49585b21b32fc1953f47233e65945bf449a9
richard <richard.zsupos@gmail.com>
2024-01-02 21:43:03 +0100

    Reverse Vowels of a String

---
+++ LeetCode75.Main/ArraysAndStrings/ReverseVowelsOfAString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace LeetCode75.Main.ArraysAndStrings;
+
+public class ReverseVowelsOfAString
+{
+    public string ReverseVowels(string s)
+    {
+        List<char> vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+        int left = 0;
+        int right = s.Length - 1;
+        char[] text = s.ToCharArray();
+
+        while (left < right)
+        {
+            while (left < s.Length && !vowels.Contains(text[left]))
+            {
+                left++;
+            }
+
+            while (right > 0 && !vowels.Contains(text[right]))
+            {
+                right--;
+            }
+
+            if (left >= right)
+            {
+                break;
+            }
+
+            (text[left], text[right]) = (text[right], text[left]);
+
+            left++;
+            right--;
+        }
+
+        return new string(text);
+    }
+}
---
+++ LeetCode75.Tests/ArraysAndStrings/ReverseVowelsOfAStringTests.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode75.Main.ArraysAndStrings;
+
+namespace LeetCode75.Tests.ArraysAndStrings;
+
+[TestFixture]
+internal class ReverseVowelsOfAStringTests
+{
+    [TestCase("hello", "holle")]
+    [TestCase("leetcode", "leotcede")]
+    public void TestExamples(string s, string expected)
+    {
+        Assert.That(new ReverseVowelsOfAString().ReverseVowels(s), Is.EqualTo(expected));
+    }
+}

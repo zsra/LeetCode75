4b9be4afb2e34353bb0461886773fc788d6faf6b
richard <richard.zsupos@gmail.com>
2024-01-02 22:56:04 +0100

    Reverse Words In a String

---
+++ LeetCode75.Main/ArraysAndStrings/ReverseWordsInAString.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode75.Main.ArraysAndStrings;
+
+public class ReverseWordsInAString
+{
+    public string ReverseWords(string s)
+    {
+        var words = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        return string.Join(" ", words.Reverse());
+    }
+}
---
+++ LeetCode75.Tests/ArraysAndStrings/ReverseWordsInAStringTests.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode75.Main.ArraysAndStrings;
+
+namespace LeetCode75.Tests.ArraysAndStrings;
+
+[TestFixture]
+internal class ReverseWordsInAStringTests
+{
+    [TestCase("the sky is blue", "blue is sky the")]
+    [TestCase("  hello world  ", "world hello")]
+    [TestCase("a good   example", "example good a")]
+    public void TestExamples(string s, string expected)
+    {
+        Assert.That(new ReverseWordsInAString().ReverseWords(s), Is.EqualTo(expected));
+    }
+}

0b01cc2421d7282cfda20c9a937114ec2be8f869
richard <richard.zsupos@gmail.com>
2024-01-03 18:32:20 +0100

    Product of Array Except Self

---
+++ LeetCode75.Main/ArraysAndStrings/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode75.Main.ArraysAndStrings;
+
+public class ProductOfArrayExceptSelf
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] asnwer = new int[nums.Length];
+        asnwer[0] = 1;
+        int suffix = 1;
+
+        for(int i = 1; i < nums.Length; i++) 
+        {
+            asnwer[i] = asnwer[i - 1] * nums[i - 1];
+        }
+
+        for (int i = nums.Length - 1; i >= 0; i--) 
+        {
+            asnwer[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return asnwer;
+    }
+}
---
+++ LeetCode75.Tests/ArraysAndStrings/ProductOfArrayExceptSelfTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode75.Main.ArraysAndStrings;
+
+namespace LeetCode75.Tests.ArraysAndStrings;
+
+[TestFixture]
+internal class ProductOfArrayExceptSelfTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums, int[] expected)
+    {
+        Assert.That(new ProductOfArrayExceptSelf().ProductExceptSelf(nums), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 2, 3, 4 }, new int[] { 24, 12, 8, 6 });
+        yield return new TestCaseData(new int[] { -1, 1, 0, -3, 3 }, new int[] { 0, 0, 9, 0, 0 });
+    }
+}

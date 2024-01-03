6ba262760d361c9644c2f1fef47a20cf26f61f64
richard <richard.zsupos@gmail.com>
2024-01-03 21:10:36 +0100

    String Compression

---
+++ LeetCode75.Main/ArraysAndStrings/StringCompression.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode75.Main.ArraysAndStrings;
+
+public class StringCompression
+{
+    public int Compress(char[] chars)
+    {
+        var i = 0;
+        var j = 0;
+
+        while (i < chars.Length)
+        {
+            var current = chars[i];
+            var counter = 0;
+
+            while (i < chars.Length && chars[i] == current)
+            {
+                i++;
+                counter++;
+            }
+
+            chars[j++] = current;
+
+            if (counter > 1)
+            {
+                foreach (var counterChar in counter.ToString())
+                {
+                    chars[j++] = counterChar;
+                }
+            }
+        }
+
+        return j;
+    }
+}
---
+++ LeetCode75.Tests/ArraysAndStrings/StringCompressionTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode75.Main.ArraysAndStrings;
+
+namespace LeetCode75.Tests.ArraysAndStrings;
+
+[TestFixture]
+internal class StringCompressionTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(char[] chars, int expected)
+    {
+        Assert.That(new StringCompression().Compress(chars), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' }, 6);
+    }
+}

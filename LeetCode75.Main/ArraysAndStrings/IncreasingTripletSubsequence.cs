d9cbe7eade5389b446725dcad5b305eebde40b3c
richard <richard.zsupos@gmail.com>
2024-01-03 19:39:15 +0100

    Increasing Triplet Subsequence

---
+++ LeetCode75.Main/ArraysAndStrings/IncreasingTripletSubsequence.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode75.Main.ArraysAndStrings;
+
+public class IncreasingTripletSubsequence
+{
+    public bool IncreasingTriplet(int[] nums)
+    {
+        if (nums == null || nums.Length < 3)
+        {
+            return false;
+        }
+
+        int a = int.MaxValue;
+        int b = int.MaxValue;
+
+        foreach (int num in nums)
+        {
+            if (num <= a)
+            {
+                a = num;
+            }
+            else if (num <= b) 
+            {
+                b = num;
+            }
+            else return true;
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode75.Tests/ArraysAndStrings/IncreasingTripletSubsequenceTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode75.Main.ArraysAndStrings;
+
+namespace LeetCode75.Tests.ArraysAndStrings;
+
+[TestFixture]
+internal class IncreasingTripletSubsequenceTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums, bool expected)
+    {
+        Assert.That(new IncreasingTripletSubsequence().IncreasingTriplet(nums), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 2, 3, 4, 5 }, true);
+        yield return new TestCaseData(new int[] { 5, 4, 3, 2, 1 }, false);
+        yield return new TestCaseData(new int[] { 2, 1, 5, 0, 4, 6 }, true);
+        yield return new TestCaseData(new int[] { 20, 100, 10, 12, 5, 13 }, true);
+        yield return new TestCaseData(new int[] { 0, 4, 2, 1, 0, -1, -3 }, false);
+    }
+}

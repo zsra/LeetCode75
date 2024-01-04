d47281aacc317825c3680ac81e549401ec352b6a
richard <richard.zsupos@gmail.com>
2024-01-04 18:39:19 +0100

    Move Zeroes

---
+++ LeetCode75.Main/TwoPointers/MoveZeroes.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode75.Main.TwoPointers;
+
+public class MoveZeroes
+{
+    public void MoveZeroesMethod(int[] nums)
+    {
+        if (nums.Length == 1) return;
+        int lastNullIndex = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                bool isNullFound = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] == 0)
+                    {
+                        lastNullIndex = j;
+                        isNullFound = true;
+                        break;
+                    }
+                }
+
+                if (isNullFound)
+                {
+                    nums[lastNullIndex] = nums[i];
+                    nums[i] = 0;
+                }    
+            }
+        }
+    }
+}
---
+++ LeetCode75.Tests/TwoPointers/MoveZeroesTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode75.Main.TwoPointers;
+
+namespace LeetCode75.Tests.TwoPointers
+{
+    internal class MoveZeroesTests
+    {
+        [TestCaseSource(nameof(GetTestCaseDatas))]
+        public void TestExamples(int[] nums, int[] expected)
+        {
+            new MoveZeroes().MoveZeroesMethod(nums);
+            Assert.That(nums, Is.EqualTo(expected));
+        }
+
+        private static IEnumerable<TestCaseData> GetTestCaseDatas()
+        {
+            yield return new TestCaseData(new int[] { 0, 1, 0, 3, 12 }, new int[] { 1, 3, 12, 0, 0 });
+            yield return new TestCaseData(new int[] { 0 }, new int[] { 0 });
+            yield return new TestCaseData(new int[] { 1 }, new int[] { 1 });
+            yield return new TestCaseData(new int[] { 1, 0 }, new int[] { 1, 0 });
+            yield return new TestCaseData(new int[] { 0, 1 }, new int[] { 1, 0 });
+            yield return new TestCaseData(new int[] { 2, 1 }, new int[] { 2, 1 });
+            yield return new TestCaseData(new int[] { 4, 2, 4, 0, 0, 3, 0, 5, 1, 0 }, new int[] { 4, 2, 4, 3, 5, 1, 0, 0, 0, 0 });
+        }
+    }
+}

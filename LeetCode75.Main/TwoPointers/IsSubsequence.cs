a98afdfdd07570f50997682d37a6002127f096b5
richard <richard.zsupos@gmail.com>
2024-01-05 17:47:03 +0100

    Is Subsequence

---
+++ LeetCode75.Main/TwoPointers/IsSubsequence.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode75.Main.TwoPointers;
+
+public class IsSubsequence
+{
+    public bool IsSubsequenceMethod(string s, string t)
+    {
+        if (string.IsNullOrEmpty(s)) return true;
+
+        int currentIndex = 0;
+
+        for (int i = 0; i < t.Length; i++) 
+        {
+            if (s[currentIndex] == t[i])
+            {
+                currentIndex++;
+
+                if(currentIndex == s.Length)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode75.Tests/TwoPointers/IsSubsequenceTests.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode75.Main.TwoPointers;
+
+namespace LeetCode75.Tests.TwoPointers;
+
+internal class IsSubsequenceTests
+{
+    [TestCase("abc", "ahbgdc", true)]
+    [TestCase("axc", "ahbgdc", false)]
+    public void TestExamples(string s, string t, bool expected)
+    {
+        Assert.That(new IsSubsequence().IsSubsequenceMethod(s, t), Is.EqualTo(expected));
+    }
+}

db3f4883ca58b7a790ab59ca76f3a568fb525218
richard <richard.zsupos@gmail.com>
2024-01-05 20:13:42 +0100

    Container With Most Water

---
+++ LeetCode75.Main/TwoPointers/ContainerWithMostWater.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode75.Main.TwoPointers;
+
+public class ContainerWithMostWater
+{
+    public int MaxArea(int[] height)
+    {
+        int max = 0;
+        int left = 0;
+        int right = height.Length - 1;
+
+        while (left < right)
+        {
+            int min = height[left] < height[right] ? height[left] : height[right];
+            int width = right - left;
+
+            if (max < min * width)
+            {
+                max = min * width;
+            }
+
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ LeetCode75.Tests/TwoPointers/ContainerWithMostWaterTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode75.Main.TwoPointers;
+
+namespace LeetCode75.Tests.TwoPointers;
+
+[TestFixture]
+internal class ContainerWithMostWaterTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] height, int expected)
+    {
+        Assert.That(new ContainerWithMostWater().MaxArea(height), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49);
+       
+    }
+}

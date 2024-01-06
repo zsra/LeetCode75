659543ec000d59448f560382549ab3c250345404
richard <richard.zsupos@gmail.com>
2024-01-06 19:00:05 +0100

    Max Number of K-Sum Pairs

---
+++ LeetCode75.Main/TwoPointers/MaxNumberOfKSumPairs.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode75.Main.TwoPointers;
+
+public class MaxNumberOfKSumPairs
+{
+    public int MaxOperations(int[] nums, int k)
+    {
+        Dictionary<int, int> counts = [];
+        int result = 0;
+
+        foreach (int i in nums)
+        {
+            if(counts.ContainsKey(k - i) && counts[k - i] > 0)
+            {
+                result++;
+                counts[k - i]--;
+            }
+            else
+            {
+                if (!counts.ContainsKey(i))
+                {
+                    counts[i] = 0;
+                }
+                counts[i]++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode75.Tests/TwoPointers/MaxNumberOfKSumPairsTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode75.Main.TwoPointers;
+
+namespace LeetCode75.Tests.TwoPointers;
+
+internal class MaxNumberOfKSumPairsTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums, int k, int expected)
+    {
+        Assert.That(new MaxNumberOfKSumPairs().MaxOperations(nums, k), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 2, 3, 4 }, 5, 2);
+        yield return new TestCaseData(new int[] { 3, 1, 3, 4, 3 }, 6, 1);
+    }
+}

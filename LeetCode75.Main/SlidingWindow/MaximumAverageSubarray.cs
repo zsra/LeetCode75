edaa3f35b4c31df71c341bf435b65eaf313085bd
richard <richard.zsupos@gmail.com>
2024-01-07 00:06:52 +0100

    Maximum Average Subarray

---
+++ LeetCode75.Main/SlidingWindow/MaximumAverageSubarray.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode75.Main.SlidingWindow;
+
+public class MaximumAverageSubarray
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        int index = 1;
+        double previousSum = nums[0..k].Sum();
+        double maxAvg = previousSum / k;
+
+        while (index + k <= nums.Length)
+        {
+            double sum = previousSum - nums[index - 1] + nums[index + k - 1];
+
+            if ((sum / k) > maxAvg)
+            {
+                maxAvg = (sum / k);
+            }
+
+            previousSum = sum;
+
+            index++;
+        }
+
+        return maxAvg;
+    }
+}
---
+++ LeetCode75.Tests/SlidingWindow/MaximumAverageSubarrayTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode75.Main.SlidingWindow;
+
+namespace LeetCode75.Tests.SlidingWindow;
+
+[TestFixture]
+internal class MaximumAverageSubarrayTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums, int k, double expected)
+    {
+        Assert.That(new MaximumAverageSubarray().FindMaxAverage(nums, k), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 12, -5, -6, 50, 3 }, 4, 12.75);
+        yield return new TestCaseData(new int[] { 5 }, 1, 5.00000);
+
+    }
+}

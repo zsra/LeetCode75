5959d3c0361528ff9b9cfc5ffd2b86dcf4036efe
richard <richard.zsupos@gmail.com>
2024-01-07 18:07:58 +0100

    Max Consecutive Ones

---
+++ LeetCode75.Main/SlidingWindow/MaxConsecutiveOnes.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode75.Main.SlidingWindow;
+
+public class MaxConsecutiveOnes
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        int i = 0, j;
+        for (j = 0; j < nums.Length; j++)
+        {
+            if (nums[j] == 0) k--;
+            if (k < 0 && nums[i++] == 0) k++;
+        }
+
+        return j - i;
+    }
+}
---
+++ LeetCode75.Tests/SlidingWindow/MaxConsecutiveOnesTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode75.Main.SlidingWindow;
+
+namespace LeetCode75.Tests.SlidingWindow;
+
+[TestFixture]
+internal class MaxConsecutiveOnesTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums, int k, int expected)
+    {
+        Assert.That(new MaxConsecutiveOnes().LongestOnes(nums, k), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, 2, 6);
+        yield return new TestCaseData(new int[] { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 }, 3, 10);
+    }
+}
+

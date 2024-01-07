f0e129c0238060c2e351c18ba54efde41560853f
richard <richard.zsupos@gmail.com>
2024-01-07 22:58:18 +0100

    Longest Subarray of 1's After Deleting One Element

---
+++ LeetCode75.Main/SlidingWindow/LongestSubarrayOfFirstAfterDeletingOneElement.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode75.Main.SlidingWindow;
+
+public class LongestSubarrayOfFirstAfterDeletingOneElement
+{
+    public int LongestSubarray(int[] nums)
+    {
+        int k = 1;
+        int i = 0, j;
+        for (j = 0; j < nums.Length; j++)
+        {
+            if (nums[j] == 0) k--;
+            if (k < 0 && nums[i++] == 0) k++;
+        }
+
+        return j - i - 1;
+    }
+}
---
+++ LeetCode75.Tests/SlidingWindow/LongestSubarrayOfFirstAfterDeletingOneElementTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode75.Main.SlidingWindow;
+
+namespace LeetCode75.Tests.SlidingWindow;
+
+[TestFixture]
+internal class LongestSubarrayOfFirstAfterDeletingOneElementTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums, int expected)
+    {
+        Assert.That(new LongestSubarrayOfFirstAfterDeletingOneElement().LongestSubarray(nums), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 1, 0, 1 }, 3);
+        yield return new TestCaseData(new int[] { 0, 1, 1, 1, 0, 1, 1, 0, 1 }, 5);
+    }
+}

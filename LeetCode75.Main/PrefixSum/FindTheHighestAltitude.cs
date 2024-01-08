db31c4546fae2634842d9a6bfb01037ea60bb409
richard <richard.zsupos@gmail.com>
2024-01-08 19:05:34 +0100

    Find The Highest Altitude

---
+++ LeetCode75.Main/PrefixSum/FindTheHighestAltitude.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode75.Main.PrefixSum;
+
+public class FindTheHighestAltitude
+{
+    public int LargestAltitude(int[] gain)
+    {
+        int max = 0;
+        int current = 0;
+
+        for (int i = 0; i < gain.Length; i++)
+        {
+            current += gain[i];
+
+            if (current > max)
+            {
+                max = current;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ LeetCode75.Tests/PrefixSum/FindTheHighestAltitudeTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode75.Main.PrefixSum;
+
+namespace LeetCode75.Tests.PrefixSum
+{
+    [TestFixture]
+    internal class FindTheHighestAltitudeTests
+    {
+        [TestCaseSource(nameof(GetTestCaseDatas))]
+        public void TestExamples(int[] gain, int expected)
+        {
+            Assert.That(new FindTheHighestAltitude().LargestAltitude(gain), Is.EqualTo(expected));
+        }
+
+        private static IEnumerable<TestCaseData> GetTestCaseDatas()
+        {
+            yield return new TestCaseData(new int[] { -5, 1, 5, 0, -7 }, 1);
+            yield return new TestCaseData(new int[] { -4, -3, -2, -1, 4, 3, 2 }, 0);
+            yield return new TestCaseData(new int[] { 44, 32, -9, 52, 23, -50, 50, 33, -84, 47, -14, 84, 36, -62, 37, 81, -36, -85, -39, 67, -63, 64, -47, 95, 91, -40, 65, 67, 92, -28, 97, 100, 81 }, 781);
+        }
+    }
+}

0314cd397dd89f3b359b55a9377192f17a228a2d
richard <richard.zsupos@gmail.com>
2024-01-08 20:46:10 +0100

    Find Pivot Index

---
+++ LeetCode75.Main/PrefixSum/FindPivotIndex.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode75.Main.PrefixSum;
+
+public class FindPivotIndex
+{
+    public int PivotIndex(int[] nums)
+    {
+        int totalSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            totalSum += nums[i];
+        }
+
+        int leftSum = 0;
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (leftSum * 2 == totalSum - nums[i])
+            {
+                return i;
+            }
+                
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
---
+++ LeetCode75.Tests/PrefixSum/FindPivotIndexTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode75.Main.PrefixSum;
+
+namespace LeetCode75.Tests.PrefixSum;
+
+[TestFixture]
+internal class FindPivotIndexTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums, int expected)
+    {
+        Assert.That(new FindPivotIndex().PivotIndex(nums), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 7, 3, 6, 5, 6 }, 3);
+        yield return new TestCaseData(new int[] { 1, 2, 3 }, -1);
+    }
+}

0894a14d47e6bf4c28af5e61ac16a928382a7a34
richard <richard.zsupos@gmail.com>
2024-01-09 20:58:17 +0100

    Find the Difference of Two Arrays

---
+++ LeetCode75.Main/HashMapAndSet/FindTheDifferenceOfTwoArrays.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode75.Main.HashMapAndSet;
+
+public class FindTheDifferenceOfTwoArrays
+{
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        var nums1Set = nums1.ToHashSet();
+        var nums2Set = nums2.ToHashSet();
+
+        nums1Set.ExceptWith(nums2.ToHashSet());
+        nums2Set.ExceptWith(nums1.ToHashSet());
+
+        return new List<IList<int>>
+        {
+            nums1Set.ToList(),
+            nums2Set.ToList()
+        };
+    }
+}
---
+++ LeetCode75.Tests/HashMapAndSet/FindTheDifferenceOfTwoArraysTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode75.Main.HashMapAndSet;
+
+namespace LeetCode75.Tests.HashMapAndSet;
+
+[TestFixture]
+internal class FindTheDifferenceOfTwoArraysTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums1, int[] nums2, IList<IList<int>> expected)
+    {
+        Assert.That(new FindTheDifferenceOfTwoArrays().FindDifference(nums1, nums2), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(
+            new int[] { 1, 2, 3 }, 
+            new int[] { 2, 4, 6 }, 
+            new List<IList<int>>() { new List<int>() { 1, 3 }, new List<int>() { 4, 6 } });
+        yield return new TestCaseData(
+            new int[] { 1, 2, 3, 3 },
+            new int[] { 1, 1, 2, 2 },
+            new List<IList<int>>() { new List<int>() { 3 }, new List<int>() {} });
+    }
+}

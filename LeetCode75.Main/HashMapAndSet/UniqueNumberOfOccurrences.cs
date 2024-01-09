db1ee92df8b66ac11ec112aa5682848397bbedf9
richard <richard.zsupos@gmail.com>
2024-01-09 22:45:17 +0100

    Unique Number of Occurrences

---
+++ LeetCode75.Main/HashMapAndSet/UniqueNumberOfOccurrences.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode75.Main.HashMapAndSet;
+
+public class UniqueNumberOfOccurrences
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        Dictionary<int, int> counts = [];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            counts[arr[i]] = counts.TryGetValue(arr[i], out int value) 
+                ? ++value 
+                : 0;
+        }
+
+        HashSet<int> collection = [];
+
+        foreach (var item in counts)
+        {
+            if(!collection.Add(item.Value))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ LeetCode75.Tests/HashMapAndSet/UniqueNumberOfOccurrencesTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode75.Main.HashMapAndSet;
+
+namespace LeetCode75.Tests.HashMapAndSet;
+
+[TestFixture]
+internal class UniqueNumberOfOccurrencesTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] arr, bool expected)
+    {
+        Assert.That(new UniqueNumberOfOccurrences().UniqueOccurrences(arr), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 1, 2, 2, 1, 1, 3 }, true);
+        yield return new TestCaseData(new int[] { 1, 2 }, false);
+        yield return new TestCaseData(new int[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 }, true);
+    }
+}

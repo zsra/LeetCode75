e4dba70d068f2fb5e4c50adaac991427399675cf
richard <richard.zsupos@gmail.com>
2024-01-10 21:09:47 +0100

    Determine if Two Strings Are Close

---
+++ LeetCode75.Main/HashMapAndSet/DetermineIfTwoStringsAreClose.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace LeetCode75.Main.HashMapAndSet;
+
+public class DetermineIfTwoStringsAreClose
+{
+    public bool CloseStrings(string word1, string word2)
+    {
+        if (word1.Length != word2.Length)
+        {
+            return false;
+        }
+
+        int[] word1Arr = new int[26];
+        int[] word2Arr = new int[26];
+
+        foreach (var item in word1)
+        {
+            word1Arr[item - 'a']++;
+        }
+
+        foreach (var item in word2)
+        {
+            word2Arr[item - 'a']++;
+        }
+
+        for (int i = 0; i < word1Arr.Length; i++)
+        {
+            if (word1Arr[i] == 0 && word2Arr[i] != 0 || word1Arr[i] != 0 && word2Arr[i] == 0)
+            {
+                return false;
+            }
+        }
+
+        Array.Sort(word1Arr);
+        Array.Sort(word2Arr);
+        
+        for (int i = 0; i < word1Arr.Length; i++)
+        {
+            if (word1Arr[i] != word2Arr[i]) return false;
+        }
+
+        return true;
+    }
+}
---
+++ LeetCode75.Tests/HashMapAndSet/DetermineIfTwoStringsAreCloseTests.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode75.Main.HashMapAndSet;
+
+namespace LeetCode75.Tests.HashMapAndSet;
+
+[TestFixture]
+internal class DetermineIfTwoStringsAreCloseTests
+{
+    [TestCase("abc", "bca", true)]
+    [TestCase("a", "aa", false)]
+    [TestCase("cabbba", "abbccc", true)]
+    public void TestExamples(string word1, string word2, bool expected)
+    {
+        Assert.That(new DetermineIfTwoStringsAreClose().CloseStrings(word1, word2), Is.EqualTo(expected));
+    }
+}

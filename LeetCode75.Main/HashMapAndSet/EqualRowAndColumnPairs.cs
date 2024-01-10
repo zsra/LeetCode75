56f8ac5022bf7e07779f6f93c4d19e33b3a88c0e
richard <richard.zsupos@gmail.com>
2024-01-10 22:14:27 +0100

    Equal Row and Column Pairs

---
+++ LeetCode75.Main/HashMapAndSet/EqualRowAndColumnPairs.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode75.Main.HashMapAndSet;
+
+public class EqualRowAndColumnPairs
+{
+    public int EqualPairs(int[][] grid)
+    {
+        int count = 0;
+        int n = grid.Length;
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                bool equal = true;
+                for (int x = 0; x < n; x++)
+                {
+                    if (grid[i][x] != grid[x][j])
+                    {
+                        equal = false;
+                        break;
+                    }
+                }
+                if (equal) count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ LeetCode75.Tests/HashMapAndSet/EqualRowAndColumnPairsTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode75.Main.HashMapAndSet;
+
+namespace LeetCode75.Tests.HashMapAndSet;
+
+[TestFixture]
+internal class EqualRowAndColumnPairsTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[][] grid, int expected)
+    {
+        Assert.That(new EqualRowAndColumnPairs().EqualPairs(grid), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[][] { [3, 2, 1], [1, 7, 6], [2, 7, 7] }, 1);
+    }
+}

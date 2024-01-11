b1ce2388571b6ffebbe869eafda673cf9d1e13c3
richard <richard.zsupos@gmail.com>
2024-01-11 20:33:28 +0100

    Asteroid Collision

---
+++ LeetCode75.Main/Stack/AsteroidCollision.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode75.Main.Stack;
+
+public class AsteroidCollision
+{
+    public int[] AsteroidCollisionMethod(int[] asteroids)
+    {
+        Stack<int> stack = [];
+
+        foreach (int asteroid in asteroids)
+        {
+            if (asteroid > 0)
+            {
+                stack.Push(asteroid);
+            }
+            else
+            {
+                while (stack.Count > 0 && stack.Peek() > 0 && stack.Peek() < Math.Abs(asteroid))
+                {
+                    stack.Pop();
+                }
+
+                if (stack.Count > 0 && stack.Peek() == Math.Abs(asteroid))
+                {
+                    stack.Pop();
+                }
+                else if ((stack.Count > 0 && stack.Peek() < 0) || stack.Count == 0)
+                {
+                    stack.Push(asteroid);
+                }
+            }
+        }
+
+        int[] ans = new int[stack.Count];
+        int idx = ans.Length - 1;
+
+        while (stack.Count > 0)
+        {
+            ans[idx--] = stack.Pop();
+        }
+
+        return ans;
+    }
+}
---
+++ LeetCode75.Tests/Stack/AsteroidCollisionTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode75.Main.Stack;
+
+namespace LeetCode75.Tests.Stack;
+
+internal class AsteroidCollisionTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] asteroids, int[] expected)
+    {
+        Assert.That(new AsteroidCollision().AsteroidCollisionMethod(asteroids), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 5, 10, -5 }, new int[] { 5, 10 });
+        yield return new TestCaseData(new int[] { 8, -8 }, new int[] { });
+        yield return new TestCaseData(new int[] { 10, 2, -5 }, new int[] { 10 });
+    }
+}

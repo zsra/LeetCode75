8627c1a021f7daed857216b4d946a944d0e7cae9
richard <richard.zsupos@gmail.com>
2024-01-11 19:29:06 +0100

    Removing Stars From a String

---
+++ LeetCode75.Main/Stack/RemovingStarsFromAString.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace LeetCode75.Main.Stack;
+
+public class RemovingStarsFromAString
+{
+    public string RemoveStars(string s)
+    {
+        Stack<char> stack = new();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] != '*')
+                stack.Push(s[i]);
+            else
+                stack.Pop();
+        }
+
+        return string.Join("", stack.Reverse());
+    }
+}
---
+++ LeetCode75.Tests/Stack/RemovingStarsFromAStringTests.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode75.Main.Stack;
+
+namespace LeetCode75.Tests.Stack;
+
+[TestFixture]
+internal class RemovingStarsFromAStringTests
+{
+    [TestCase("leet**cod*e", "lecoe")]
+    [TestCase("erase*****", "")]
+    public void TestExamples(string s, string expected)
+    {
+        Assert.That(new RemovingStarsFromAString().RemoveStars(s), Is.EqualTo(expected));
+    }
+}

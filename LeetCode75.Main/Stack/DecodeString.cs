6e905dc136e1b97abeafe72ac10f174d6873dde7
richard <richard.zsupos@gmail.com>
2024-01-12 00:10:01 +0100

    Decode String

---
+++ LeetCode75.Main/Stack/DecodeString.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace LeetCode75.Main.Stack;
+public class DecodeString
+{
+    public string DecodeStringMethod(string s)
+    {
+        Stack<string> strings = new();
+        Stack<int> numbers = new();
+
+        StringBuilder builder = new();
+        int currentNumber = 0;
+
+        foreach (char c in s)
+        {
+            if (c == '[')
+            {
+                strings.Push(builder.ToString());
+                numbers.Push(currentNumber);
+                builder.Clear();
+                currentNumber = 0;
+            }
+            else if (c == ']')
+            {
+                string currentString = builder.ToString();
+                builder.Clear();
+                builder.Append(strings.Pop() + string.Join("", Enumerable.Repeat(currentString, numbers.Pop())));
+            }
+            else if (int.TryParse(c.ToString(), out int n))
+            {
+                currentNumber = currentNumber * 10 + n;
+            }
+            else
+            {
+                builder.Append(c);
+            }
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ LeetCode75.Tests/Stack/DecodeStringTests.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode75.Main.Stack;
+
+namespace LeetCode75.Tests.Stack;
+
+[TestFixture]
+internal class DecodeStringTests
+{
+    [TestCase("3[a]2[bc]", "aaabcbc")]
+    [TestCase("3[a2[c]]", "accaccacc")]
+    [TestCase("2[abc]3[cd]ef", "abcabccdcdcdef")]
+    public void TestExamples(string s, string expected)
+    {
+        Assert.That(new DecodeString().DecodeStringMethod(s), Is.EqualTo(expected));
+    }
+}

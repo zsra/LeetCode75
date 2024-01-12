ca5bfe6e3561c67ad9273d5ba758851e60165950
richard <richard.zsupos@gmail.com>
2024-01-12 23:06:42 +0100

    Number of Recent Calls

---
+++ LeetCode75.Main/Queue/RecentCounter.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode75.Main.Queue;
+
+public class RecentCounter
+{
+    private readonly Queue<int> _queue = new();
+
+    public RecentCounter()
+    {
+        _queue = new();
+    }
+
+    public int Ping(int t)
+    {
+        _queue.Enqueue(t);
+        while (_queue.Peek() < t - 3000)
+        {
+            _queue.Dequeue();
+        }
+
+        return _queue.Count;
+    }
+}

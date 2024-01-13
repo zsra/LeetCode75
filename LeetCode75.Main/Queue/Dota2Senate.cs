a89e56357f9838679c54c74a587d606855113abe
richard <richard.zsupos@gmail.com>
2024-01-13 11:31:00 +0100

    Dota2 Senate

---
+++ LeetCode75.Main/Queue/Dota2Senate.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode75.Main.Queue;
+
+public class Dota2Senate
+{
+    public string PredictPartyVictory(string senate)
+    {
+        Queue<char> senators = new(senate);
+        int countR = senate.Count(x => x == 'R');
+        int countD = senate.Length - countR;
+
+        int scale = 0;
+
+        while (countR > 0 && countD > 0)
+        {
+            char senator = senators.Dequeue();
+
+            if (senator == 'R')
+            {
+                if (scale >= 0)
+                {
+                    countD--;
+                    senators.Enqueue(senator);
+                }
+                scale++;
+            }
+            else
+            {
+                if (scale <= 0)
+                {
+                    countR--;
+                    senators.Enqueue(senator);
+                }
+                scale--;
+            }
+        }
+
+        return countR == 0 ? "Dire" : "Radiant";
+    }
+}
---
+++ LeetCode75.Tests/Queue/Dota2SenateTests.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode75.Main.Queue;
+
+namespace LeetCode75.Tests.Queue;
+
+[TestFixture]
+internal class Dota2SenateTests
+{
+    [TestCase("RD", "Radiant")]
+    [TestCase("RDD", "Dire")]
+    public void TestExamples(string senate, string expected)
+    {
+        Assert.That(new Dota2Senate().PredictPartyVictory(senate), Is.EqualTo(expected));
+    }
+}

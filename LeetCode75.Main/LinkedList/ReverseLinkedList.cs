7ccaba26419fbf69c902f29365bfd758e19f8082
richard <richard.zsupos@gmail.com>
2024-01-13 22:50:07 +0100

    Reverse Linked List

---
+++ LeetCode75.Main/LinkedList/ReverseLinkedList.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode75.Main.LinkedList;
+
+public class ReverseLinkedList
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if( head == null || head.next == null )
+        {
+            return head;
+        }
+
+        List<int> values = [];
+
+        while (head != null)
+        {
+            values.Add(head.val);
+            head = head.next;
+        }
+
+        ListNode current = new(values[values.Count - 1]);
+        head = current;
+
+        for (int i = values.Count - 2; i >= 0; i--)
+        {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+}

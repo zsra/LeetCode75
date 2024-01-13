4372e5be10c7b3a0ef4380a661d901b5fb02a6d2
richard <richard.zsupos@gmail.com>
2024-01-13 18:01:12 +0100

    Delete the Middle Node of a Linked List

---
+++ LeetCode75.Main/LinkedList/DeleteTheMiddleNodeOfALinkedList.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode75.Main.LinkedList;
+
+public class DeleteTheMiddleNodeOfALinkedList
+{
+    public ListNode DeleteMiddle(ListNode head)
+    {
+        int length = 0;
+
+        ListNode current = head;
+        while (current != null)
+        {
+            length++;
+            current = current.next;
+        }
+
+        int middle = length / 2;
+
+        ListNode middleNode = head;
+        ListNode prev = null;
+
+        for (int i = 0; i < middle; i++)
+        {
+            prev = middleNode;
+            middleNode = middleNode.next;
+        }
+
+        if (prev == null)
+        {
+            return null;
+        }
+
+        prev.next = middleNode.next;
+        return head;
+    }
+}

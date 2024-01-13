3e0b2d3a5d11fd8206c3f0d3548af341ac917ba9
richard <richard.zsupos@gmail.com>
2024-01-13 23:40:26 +0100

    Maximum Twin Sum of a Linked List

---
+++ LeetCode75.Main/LinkedList/MaximumTwinSumOfALinkedList.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode75.Main.LinkedList;
+
+public class MaximumTwinSumOfALinkedList
+{
+    public int PairSum(ListNode head)
+    {
+        List<int> datas = new();
+        
+        while (head != null)
+        {
+            datas.Add(head.val);
+            head = head.next;
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < datas.Count / 2; i++)
+        {
+            if (datas[i] + datas[datas.Count - i - 1] > sum)
+            {
+                sum = datas[i] + datas[datas.Count - i - 1];
+            }
+        }
+
+        return sum;
+    }
+}

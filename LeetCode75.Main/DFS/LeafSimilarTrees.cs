1b9ff557c2d1390c8ad264504a28130b52fdd74b
richard <richard.zsupos@gmail.com>
2024-01-14 15:28:20 +0100

    Leaf-Similar Trees

---
+++ LeetCode75.Main/DFS/LeafSimilarTrees.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Linq;
+
+namespace LeetCode75.Main.DFS;
+
+public class LeafSimilarTrees
+{
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        List<int> first = [];
+        List<int> second = [];
+
+        getLeafNodes(root1, first);
+        getLeafNodes(root2, second);
+
+        return first.SequenceEqual(second);
+    }
+
+    static void getLeafNodes(TreeNode root, List<int> collection)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        if (root.left == null && root.right == null)
+        {
+            collection.Add(root.val);
+            return;
+        }
+
+        if (root.left != null)
+        {
+            getLeafNodes(root.left, collection);
+        }
+
+        if (root.right != null)
+        {
+            getLeafNodes(root.right, collection);
+        }
+    }
+}

5a3e1d356d312c58477d9ce0a59e1a7a74ea3485
richard <richard.zsupos@gmail.com>
2024-01-28 18:18:06 +0100

    Path Sum III

---
+++ LeetCode75.Main/DFS/PathSum3.cs
@@ -0,0 +1,31 @@
+﻿
+namespace LeetCode75.Main.DFS;
+
+public class PathSum3
+{
+    public int PathSum(TreeNode root, int targetSum)
+    {
+        return root == null
+            ? 0
+            : DFS(root, targetSum)
+            + PathSum(root.left, targetSum)
+            + PathSum(root.right, targetSum);
+    }
+
+    private static int DFS(TreeNode root, long targetSum)
+    {
+        var s = root.val == targetSum ? 1 : 0;
+
+        if (root.left != null)
+        {
+            s += DFS(root.left, targetSum - root.val);
+        }
+
+        if (root.right != null)
+        {
+            s += DFS(root.right, targetSum - root.val);
+        }
+
+        return s;
+    }
+}

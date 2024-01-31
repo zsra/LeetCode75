b99bf9db27e1db2743f9b5adef8d8eba19120428
richard <richard.zsupos@gmail.com>
2024-01-31 19:25:48 +0100

    Lowest Common Ancestor of a Binary Tree

---
+++ LeetCode75.Main/DFS/LongestZigZagPathInABinaryTree.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode75.Main.DFS;
+
+public class LongestZigZagPathInABinaryTree
+{
+    public int LongestZigZag(TreeNode root)
+    {
+        return Math.Max(DFS(root.left, true, 0), DFS(root.right, false, 0));
+    }
+
+    static int DFS(TreeNode node, bool isLeft, int count)
+    {
+        return node == null
+            ? count
+            : isLeft
+            ? Math.Max(DFS(node.left, true, 0), DFS(node.right, false, count + 1))
+            : Math.Max(DFS(node.left, true, count + 1), DFS(node.right, false, 0));
+    }
+}
---
+++ LeetCode75.Main/DFS/LowestCommonAncestorOfABinaryTree.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode75.Main.DFS;
+
+public class LowestCommonAncestorOfABinaryTree
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if ((root == null) || (root == p) || (root == q))
+        {
+            return root;
+        }
+
+        var leftLCA = LowestCommonAncestor(root.left, p, q);
+        var rightLCA = LowestCommonAncestor(root.right, p, q);
+
+        if (leftLCA == null)
+        {
+            return rightLCA;
+        }
+        else if (rightLCA == null)
+        {
+            return leftLCA;
+        }
+        else
+        {
+            return root;
+        }
+    }
+}

5b08d64776ad280e3dd5d1e9652b9ff429354398
richard <richard.zsupos@gmail.com>
2024-02-08 20:58:13 +0100

    Maximum Level Sum of a Binary Tree

---
+++ LeetCode75.Main/BFS/MaximumLevelSumOfABinaryTree.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode75.Main.BFS;
+
+internal class MaximumLevelSumOfABinaryTree
+{
+    public int MaxLevelSum(TreeNode root)
+    {
+        var traverse = new Queue<TreeNode>();
+        traverse.Enqueue(root);
+
+        var maxLevel = 1;
+        var maxLevelSum = root.val;
+        var currentLevel = 0;
+
+        while (traverse.Count != 0)
+        {
+            currentLevel++;
+            var levelCount = traverse.Count;
+            var currentLevelSum = 0;
+
+            for (int i = 0; i < levelCount; i++)
+            {
+                var node = traverse.Dequeue();
+
+                if (node.left != null) traverse.Enqueue(node.left);
+                if (node.right != null) traverse.Enqueue(node.right);
+
+                currentLevelSum += node.val;
+            }
+
+            if (maxLevelSum < currentLevelSum)
+            {
+                maxLevelSum = currentLevelSum;
+                maxLevel = currentLevel;
+            }
+        }
+
+        return maxLevel;
+    }
+}

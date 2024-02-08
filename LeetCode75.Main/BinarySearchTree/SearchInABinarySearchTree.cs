e47eb2121568bea1915ac88f765b0fd269e69922
richard <richard.zsupos@gmail.com>
2024-02-08 23:33:22 +0100

    Search in a Binary Search Tree

---
+++ LeetCode75.Main/BinarySearchTree/SearchInABinarySearchTree.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode75.Main.BinarySearchTree;
+
+internal class SearchInABinarySearchTree
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        while (root != null)
+        {
+            if (root.val == val)
+            {
+                return root;
+            }
+
+            else if (val < root.val)
+            {
+                root = root.left;
+            }
+
+            else
+            {
+                root = root.right;
+            }
+        }
+
+        return null;
+    }
+}

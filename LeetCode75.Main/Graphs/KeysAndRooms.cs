c352dee4aae5d638a970e0f81df16a1ae2a3b4fe
richard <richard.zsupos@gmail.com>
2024-02-10 00:25:26 +0100

    Keys and Rooms

---
+++ LeetCode75.Main/Graphs/KeysAndRooms.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode75.Main.Graphs;
+
+internal class KeysAndRooms
+{
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        var visited = new bool[rooms.Count];
+
+        var stack = new Stack<int>();
+        stack.Push(0);
+        
+        while (stack.Count > 0)
+        {
+            int curRoom = stack.Pop();
+            visited[curRoom] = true;
+            
+            foreach (int room in rooms[curRoom])
+            {
+                if (!visited[room])
+                {
+                    stack.Push(room);
+                }
+            }
+        }
+
+        return visited.Min();
+    }
+}

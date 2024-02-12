919733e28f0531f1afde75327810873f2f2051bb
richard <richard.zsupos@gmail.com>
2024-02-12 20:22:58 +0100

    Kth Largest Element in an Array

---
+++ LeetCode75.Main/Heap/KthLargestElementInAnArray.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode75.Main.Heap;
+
+public class KthLargestElementInAnArray
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        PriorityQueue<int, int> priorityQueue = new(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            priorityQueue.Enqueue(nums[i], nums[i]);
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < k; i++) 
+        {
+            result = priorityQueue.Dequeue();
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode75.Tests/Heap/KthLargestElementInAnArrayTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode75.Main.Heap;
+
+namespace LeetCode75.Tests.Heap;
+
+[TestFixture]
+internal class KthLargestElementInAnArrayTests
+{
+    [TestCaseSource(nameof(GetTestCaseDatas))]
+    public void TestExamples(int[] nums, int k, int expected)
+    {
+        Assert.That(new KthLargestElementInAnArray().FindKthLargest(nums, k), Is.EqualTo(expected));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCaseDatas()
+    {
+        yield return new TestCaseData(new int[] { 3, 2, 1, 5, 6, 4 }, 2, 5);
+        yield return new TestCaseData(new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4, 4);
+    }
+}

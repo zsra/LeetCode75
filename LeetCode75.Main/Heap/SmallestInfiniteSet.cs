9d0446f3e63f74648b24b4f7c1957549c8d36b28
richard <richard.zsupos@gmail.com>
2024-02-12 20:26:44 +0100

    Smallest Number in Infinite Set

---
+++ LeetCode75.Main/Heap/SmallestInfiniteSet.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode75.Main.Heap;
+
+internal class SmallestInfiniteSet
+{
+    private readonly PriorityQueue<int, int> pq;
+
+    public SmallestInfiniteSet()
+    {
+        pq = new PriorityQueue<int, int>(Enumerable.Range(1, 1000).Select(x => (x, x)));
+    }
+
+    public int PopSmallest()
+    {
+        int smallest = pq.Dequeue();
+        
+        while (pq.Count > 0 && pq.Peek() == smallest)
+        {
+            pq.Dequeue();
+        }
+
+        return smallest;
+    }
+
+    public void AddBack(int num)
+    {
+        pq.Enqueue(num, num);
+    }
+}

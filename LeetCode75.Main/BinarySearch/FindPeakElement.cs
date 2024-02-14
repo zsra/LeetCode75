56b897c77676dc13d9f7d4e3dd89d69d400444a1
richard <richard.zsupos@gmail.com>
2024-02-14 19:34:27 +0100

    Find Peak Element

---
+++ LeetCode75.Main/BinarySearch/FindPeakElement.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode75.Main.BinarySearch;
+
+internal class FindPeakElement
+{
+    public int FindPeakElementMethod(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left + 1 < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1])
+            {
+                left = mid;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return nums[left] > nums[right] ? left : right;
+    }
+}

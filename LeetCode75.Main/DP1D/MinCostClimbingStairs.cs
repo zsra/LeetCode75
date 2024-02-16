57a2674f44eb10eab02ac259071acd2ad474413a
richard <richard.zsupos@gmail.com>
2024-02-16 17:44:18 +0100

    Min Cost Climbing Stairs

---
+++ LeetCode75.Main/DP1D/MinCostClimbingStairs.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode75.Main.DP1D;
+
+internal class MinCostClimbingStairs
+{
+    public int MinCostClimbingStairsMethod(int[] cost)
+    {
+        int n = cost.Length;
+        int[] dp = new int[n + 1];
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = Math.Min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[n];
+    }
+}

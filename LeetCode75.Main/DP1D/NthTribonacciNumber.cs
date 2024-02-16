3a5807c3db6988566278f11c45a832793eb175ad
richard <richard.zsupos@gmail.com>
2024-02-16 17:34:35 +0100

    N-th Tribonacci Number

---
+++ LeetCode75.Main/DP1D/NthTribonacciNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode75.Main.DP1D;
+
+internal class NthTribonacciNumber
+{
+    public int Tribonacci(int n)
+    {
+        switch (n)
+        {
+            case 1:
+            case 2:
+                return 1;
+        }
+
+        int val0 = 0;
+        int val1 = 1;
+        int val2 = 1;
+        int nextNumber = 0;
+        
+        for (int i = 2; i < n; i++)
+        {
+            nextNumber = val0 + val1 + val2;
+            val0 = val1;
+            val1 = val2;
+            val2 = nextNumber;
+        }
+
+        return nextNumber;
+    }
+}

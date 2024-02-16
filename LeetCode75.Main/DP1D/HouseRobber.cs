e0298460fdcf611b71d7a24b534877cb1d2ef11b
richard <richard.zsupos@gmail.com>
2024-02-16 17:52:54 +0100

    House Robber

---
+++ LeetCode75.Main/DP1D/HouseRobber.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode75.Main.DP1D;
+
+internal class HouseRobber
+{
+    public int Rob(int[] nums)
+    {
+        int prevMax = 0;
+        int currMax = 0;
+
+        foreach (int num in nums)
+        {
+            int temp = currMax;
+            currMax = Math.Max(prevMax + num, currMax);
+            prevMax = temp;
+        }
+
+        return currMax;
+    }
+}

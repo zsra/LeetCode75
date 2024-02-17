fae2a45a17de39ebb54db52e23a2b66e92cb7187
richard <richard.zsupos@gmail.com>
2024-02-17 18:33:26 +0100

    Best Time to Buy and Sell Stock with Transaction Fee

---
+++ LeetCode75.Main/DPMultidimensional/BestTimeToBuyAndSellStockWithTransactionFee.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode75.Main.DPMultidimensional;
+
+internal class BestTimeToBuyAndSellStockWithTransactionFee
+{
+    public int MaxProfit(int[] prices, int fee)
+    {
+        if (prices.Length <= 1)
+        {
+            return 0;
+        }
+
+        int saving = -prices[0];
+        int profit = 0;
+        
+        for (int i = 1; i < prices.Length; i++)
+        {
+            profit = Math.Max(profit, saving + prices[i] - fee);
+            saving = Math.Max(saving, profit - prices[i]);
+        }
+
+        return profit;
+    }
+}

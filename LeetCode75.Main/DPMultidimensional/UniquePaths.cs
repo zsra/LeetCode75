533480eec19a30128150a8e930d55ae8427b8492
richard <richard.zsupos@gmail.com>
2024-02-17 17:18:14 +0100

    Unique Paths

---
+++ LeetCode75.Main/DPMultidimensional/UniquePaths.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode75.Main.DPMultidimensional;
+
+internal class UniquePaths
+{
+    public int UniquePathsMethod(int m, int n)
+    {
+        long ans = 1;
+        
+        for (int i = 1; i <= m - 1; i++)
+        {
+            ans = ans * (n - 1 + i) / i;
+        }
+
+        return (int)ans;
+    }
+}

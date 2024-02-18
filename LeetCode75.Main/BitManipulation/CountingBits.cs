95bef6782bfc8cbd9425efcd432ec7c4f829ff62
richard <richard.zsupos@gmail.com>
2024-02-18 13:48:49 +0100

    Counting Bits

---
+++ LeetCode75.Main/BitManipulation/CountingBits.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode75.Main.BitManipulation;
+
+internal class CountingBits
+{
+    public int[] CountBitsMethod(int n)
+    {
+        int[] numberOf1BitsArray = new int[n + 1];
+        for (int i = 0; i < n + 1; i++)
+        {
+            string binary = Convert.ToString(i, 2);
+            numberOf1BitsArray[i] = binary.Count(x => x == '1');
+        }
+
+        return numberOf1BitsArray;
+    }
+}

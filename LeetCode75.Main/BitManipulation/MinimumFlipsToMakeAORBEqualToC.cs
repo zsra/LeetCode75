75ed44bafdd2ad635ab6bdd2a9015a54498d02e2
richard <richard.zsupos@gmail.com>
2024-02-18 13:57:36 +0100

    Minimum Flips to Make a OR b Equal to c

---
+++ LeetCode75.Main/BitManipulation/MinimumFlipsToMakeAORBEqualToC.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode75.Main.BitManipulation;
+
+internal class MinimumFlipsToMakeAORBEqualToC
+{
+    public int MinFlips(int a, int b, int c)
+    {
+        int flips = 0;
+
+        for (int i = 0; i < 32; i++)
+        {
+            int bitA = (a >> i) & 1;
+            int bitB = (b >> i) & 1;
+            int bitC = (c >> i) & 1;
+
+            if (bitC == 1)
+            {
+                if (bitA == 0 && bitB == 0)
+                {
+                    flips++;
+                }
+            }
+            else
+            {
+                flips += bitA + bitB;
+            }
+        }
+
+        return flips;
+    }
+}

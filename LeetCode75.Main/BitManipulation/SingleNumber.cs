d6544cea41e7e9a1e90d8807dc942cf79fac64ee
richard <richard.zsupos@gmail.com>
2024-02-18 13:50:56 +0100

    Single Number

---
+++ LeetCode75.Main/BitManipulation/SingleNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode75.Main.BitManipulation;
+
+internal class SingleNumber
+{
+    public int SingleNumberMethod(int[] nums)
+    {
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i += 2)
+        {
+            if (i == nums.Length - 1 || nums[i] != nums[i + 1])
+            {
+                return nums[i];
+            }
+        }
+
+        return 0;
+    }
+}

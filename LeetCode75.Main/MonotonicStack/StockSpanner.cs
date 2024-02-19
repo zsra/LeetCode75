74b137572f554e2381a121a6b80f9178d409b826
richard <richard.zsupos@gmail.com>
2024-02-19 18:03:58 +0100

    Online Stock Span

---
+++ LeetCode75.Main/MonotonicStack/StockSpanner.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode75.Main.MonotonicStack;
+
+public class StockSpanner
+{
+    private readonly Stack<(int price, int day)> stack;
+    private int day;
+
+    public StockSpanner() => stack = new();
+
+    public int Next(int price)
+    {
+        day++;
+        
+        while (stack.Count > 0 && stack.Peek().price <= price)
+        {
+            stack.Pop();
+        }
+
+        var span = day - (stack.Count > 0 ? stack.Peek().day : 0);
+        stack.Push((price, day));
+
+        return span;
+    }
+}

8e194f2a983cffc5b002bdaa56399320da2ac0bc
richard <richard.zsupos@gmail.com>
2024-02-19 17:51:48 +0100

    Minimum Number of Arrows to Burst Balloons

---
+++ LeetCode75.Main/Intervals/MinimumNumberOfArrowsToBurstBalloons.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode75.Main.Intervals;
+
+internal class MinimumNumberOfArrowsToBurstBalloons
+{
+    public int FindMinArrowShots(int[][] points)
+    {
+        if (points.Length <= 1)
+        {
+            return 1;
+        }
+
+        Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
+        
+        int count = 1;
+        int end = points[0][1];
+
+        for (int i = 1; i < points.Length; i++)
+        {
+            if (points[i][0] > end)
+            {
+                count++;
+                end = points[i][1];
+            }
+        }
+
+        return count;
+    }
+}

8d33dc87a077295795a81d6bd740b5ea4225aa7e
richard <richard.zsupos@gmail.com>
2024-02-19 17:59:46 +0100

    Daily Temperatures

---
+++ LeetCode75.Main/MonotonicStack/DailyTemperatures.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode75.Main.MonotonicStack;
+
+internal class DailyTemperatures
+{
+    public int[] DailyTemperaturesMethod(int[] temperatures)
+    {
+        int[] results = new int[temperatures.Length];
+        Stack<int> stack = new();
+        
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
+            {
+                int index = stack.Pop();
+                results[index] = i - index;
+            }
+
+            stack.Push(i);
+        }
+
+        return results;
+    }
+}
